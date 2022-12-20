ba94c1b7fb9716f4dbafd9242d926d60e3413884
Melle Bosboom <jubiman@users.noreply.github.com>
2022-12-20 21:34:19 +0100

    forgor to add docs lmao

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.jubiman.customplayerlib"},{"l":"com.jubiman.customplayerlib.patch"},{"l":"com.jubiman.customplayerlib.tick"}];updateSearchResults();
